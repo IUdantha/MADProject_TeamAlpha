a20fb4657f5e179217a0ceab41cc692cad9e40c0
IUdantha <udanthaisuru@gmail.com>
2023-05-08 21:46:54 +0530

    Calculator created

---
+++ app/src/main/java/com/example/candidate_account_uis/Algorithm/Calculator.kt
@@ -0,0 +1,47 @@
+package com.example.candidate_account_uis.Algorithm
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.candidate_account_uis.R
+import com.example.candidate_account_uis.candidateActivities.ProfileSettingsActivity
+
+class Calculator : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+
+
+        val salary = findViewById<EditText>(R.id.salary)
+        val noDays = findViewById<EditText>(R.id.noDays)
+        val hourDay = findViewById<EditText>(R.id.hourDay)
+        val calBtn = findViewById<Button>(R.id.calBtn)
+        val calValue = findViewById<TextView>(R.id.calValue)
+
+
+
+        // Set click listener for the Calculate button
+        calBtn.setOnClickListener {
+            // Get input values from EditText fields
+            val salary = salary.text.toString().toFloat()
+            val noDays = noDays.text.toString().toFloat()
+            val hourDay = hourDay.text.toString().toFloat()
+
+            // Calculate the value
+            val calResult = salary * noDays * hourDay
+
+            // Format the result to 2 decimal points
+            val formattedResult = String.format("%.2f", calResult)
+
+            // Set the result to the TextView
+            calValue.text = "Rs. $formattedResult"
+        }
+
+
+    }
+}
