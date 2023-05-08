913f297e33aab39be94b1e0d8f6d61097caa9d40
IT21468360 <it21468360@my.sliit.lk>
2023-05-08 18:37:16 +0530

    Company Profile CRUD updated

--- app/src/main/java/com/example/candidate_account_uis/company/CompanyDetailForm.kt
+++ app/src/main/java/com/example/candidate_account_uis/company/CompanyDetailForm.kt
@@ -1,18 +1,17 @@
 package com.example.candidate_account_uis.company
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputType
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.Toast
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity
 import com.example.candidate_account_uis.R
 import com.example.candidate_account_uis.company.adapters.companyDetailsAdapter
 import com.example.candidate_account_uis.company.entities.companyDetails
 import com.google.firebase.database.FirebaseDatabase
-
 
 
 class CompanyDetailForm : AppCompatActivity() {
@@ -20,6 +19,7 @@ class CompanyDetailForm : AppCompatActivity() {
     // Initialize Firebase Realtime Database reference
     val myDbRef = FirebaseDatabase.getInstance().getReference("companyDetails")
     val adapter = companyDetailsAdapter()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,21 +38,34 @@ class CompanyDetailForm : AppCompatActivity() {
             val comContact = findViewById<EditText>(R.id.edtComContact).text.toString().toInt()
             val comEmail = findViewById<EditText>(R.id.edtComEmail).text.toString()
             val comDescription = findViewById<EditText>(R.id.edtComDescription).text.toString()
+            val comPassword = findViewById<EditText>(R.id.edtComPassword).text.toString()
 
-            val myData = companyDetails(name = comName, contact = comContact, email = comEmail, description = comDescription)
+            val myData = companyDetails(name = comName, contact = comContact, email = comEmail, description = comDescription, password = comPassword)
 
             myDbRef.push().setValue(myData)
 
+            Toast.makeText(this, "You have created an account successfully!", Toast.LENGTH_SHORT).show();
 
             // Navigate to the display activity
-            val intent = Intent(this, CompanyProfile::class.java)
-            intent.putExtra("comName", myData.name)
-            intent.putExtra("comContact", myData.contact)
-            intent.putExtra("comEmail", myData.email)
-            intent.putExtra("comDescription", myData.description)
+            val intent = Intent(this, ComLogin::class.java)
             startActivity(intent)
+//            val intent = Intent(this, CompanyProfile::class.java)
+//            intent.putExtra("comName", myData.name)
+//            intent.putExtra("comContact", myData.contact)
+//            intent.putExtra("comEmail", myData.email)
+//            intent.putExtra("comDescription", myData.description)
+//            intent.putExtra("comPassword", myData.password)
+//            startActivity(intent)
 
         }
+
+        val loginComAcc = findViewById<TextView>(R.id.loginComAcc)
+        loginComAcc.setOnClickListener(View.OnClickListener() {
+
+                val intent = Intent(this, ComLogin::class.java)
+                startActivity(intent)
+
+        })
     }
 
 
